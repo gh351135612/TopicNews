5258d4c964b8dc9a617347cb292ef6d451606e8f
chenxl <shannonc@163.com>
2017-01-13 14:42:58 +0800

    加入mybatis配置文件

---
+++ topic-web/topic-web-service/src/main/java/service/ServiceTest.java
@@ -0,0 +1,16 @@
+package service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author chenxl
+ * @Date 2017/1/13 10:17
+ * @Describle
+ */
+@Service
+public class ServiceTest {
+
+
+
+
+}
